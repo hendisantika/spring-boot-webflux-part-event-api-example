3fb8e213c13ed48ae6769faa72e9c6233c5c09a2
hendisantika <hendisantika@yahoo.co.id>
2024-03-19 09:45:52 +0700

    Add handleMultiValueMap in FileUploadController 🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootwebfluxparteventapiexample/controller/FileUploadController.java
+++ src/main/java/id/my/hendisantika/springbootwebfluxparteventapiexample/controller/FileUploadController.java
@@ -6,11 +6,15 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.codec.multipart.FilePart;
+import org.springframework.http.codec.multipart.Part;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import reactor.core.publisher.Mono;
 
 import java.io.File;
 import java.io.IOException;
@@ -18,6 +22,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.List;
 import java.util.Map;
 
 import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
@@ -94,4 +99,15 @@ public class FileUploadController {
         );
         return ok().body(result);
     }
+
+    @PostMapping(value = "upload-with-multi-value-map", consumes = MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Mono<List<String>>> handleMultiValueMap(
+            @RequestBody Mono<MultiValueMap<String, Part>> parts) {
+        log.debug("handling multi values: {}", parts);
+        var partNames = parts.map
+                        (p -> p.keySet().stream().map(key -> p.getFirst(key).name()).toList())
+                .log();
+        return ok().body(partNames);
+    }
+
 }
