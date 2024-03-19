81b2949200dc70770c6ac1f3fc0d0e72da0c9fda
hendisantika <hendisantika@yahoo.co.id>
2024-03-19 09:44:10 +0700

    Add FileUploadController 🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootwebfluxparteventapiexample/controller/FileUploadController.java
@@ -0,0 +1,20 @@
+package id.my.hendisantika.springbootwebfluxparteventapiexample.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-webflux-part-event-api-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/19/24
+ * Time: 09:44
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@Slf4j
+@SuppressWarnings("unused")
+public class FileUploadController {
+}
