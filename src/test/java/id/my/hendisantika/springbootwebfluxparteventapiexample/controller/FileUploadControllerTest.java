4f61f40e90af45cb8f19584165df363e75d839fc
hendisantika <hendisantika@yahoo.co.id>
2024-03-19 09:47:04 +0700

    Add testHandleSimpleFileUpload in FileUploadControllerTest 🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/springbootwebfluxparteventapiexample/controller/FileUploadControllerTest.java
+++ src/test/java/id/my/hendisantika/springbootwebfluxparteventapiexample/controller/FileUploadControllerTest.java
@@ -1,8 +1,15 @@
 package id.my.hendisantika.springbootwebfluxparteventapiexample.controller;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
+
+import static org.springframework.http.MediaType.MULTIPART_FORM_DATA;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,10 +25,26 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 public class FileUploadControllerTest {
 
     @Autowired
-    FileUploadController fileUploadController;
+    private FileUploadController fileUploadController;
 
     @Autowired
-    WebTestClient client;
+    private WebTestClient client;
 
 
+    @Test
+    public void testHandleSimpleFileUpload() {
+        MultipartBodyBuilder multipartBodyBuilder = new MultipartBodyBuilder();
+        multipartBodyBuilder.part("name", "test");
+        multipartBodyBuilder.part("file", new ClassPathResource("spring.png"), MediaType.IMAGE_PNG);
+        var multipartBody = multipartBodyBuilder.build();
+        this.client
+                .post().uri("/simple-form-upload")
+                .contentType(MULTIPART_FORM_DATA)
+                .body(BodyInserters.fromMultipartData(multipartBody))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.name").isEqualTo("test")
+                .jsonPath("$.filename").isEqualTo("spring.png");
+    }
 }
