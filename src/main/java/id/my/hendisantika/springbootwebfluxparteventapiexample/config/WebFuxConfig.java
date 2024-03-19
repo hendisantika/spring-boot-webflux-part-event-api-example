bff032260a066ebcb95e09e3065332f8838f4cde
hendisantika <hendisantika@yahoo.co.id>
2024-03-19 09:42:23 +0700

    Add WebFuxConfig 🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootwebfluxparteventapiexample/config/WebFuxConfig.java
@@ -0,0 +1,18 @@
+package id.my.hendisantika.springbootwebfluxparteventapiexample.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-webflux-part-event-api-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/19/24
+ * Time: 09:42
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class WebFuxConfig implements WebFluxConfigurer {
+}
