3a0bc0074baf782c6e7729e7cfaeef1bd95f54ab
hendisantika <hendisantika@yahoo.co.id>
2024-03-19 09:39:47 +0700

    Create starter project 🌀✏️📗🐧🐳

---
+++ README.md
@@ -0,0 +1,1 @@
+# spring-boot-webflux-part-event-api-example
---
+++ src/test/java/id/my/hendisantika/springbootwebfluxparteventapiexample/SpringBootWebfluxPartEventApiExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootwebfluxparteventapiexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebfluxPartEventApiExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
